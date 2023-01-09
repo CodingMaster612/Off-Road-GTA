0581cca0aa9072bc1b2ad24cdbe3674b84c7dec0
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-09 13:04:45 -0600

    log

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -28,15 +28,18 @@ export default function HomePage() {
                <img className="Open-Road" src={img} />
                <div className="box">
                   <div className="input-wrapper">
-                     <div className="user-box">
-                           <input />
+                     <div className="input-box">
+                        
+                        <input type="text" placeholder=""id="lname" name="lname" />
                      </div>
-                     <div className="password-box">
-                           <input />
+                     <div className="input-box">
+                        <input type="text" placeholder=""id="lname" name="lname" />
                      </div>
+                     
                   </div>
-
-
+                  <h1>User</h1>
+                  <h2>Password</h2>
+                  <button class="button">Login</button>
                </div>
             </div>
 
